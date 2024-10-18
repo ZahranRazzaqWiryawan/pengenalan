da363e82582c2c6609860bc65a995568286213a3
ZahranRazzaqWiryawan <supresorakm@gmail.com>
2024-10-18 16:30:23 +0700

    Add files via upload

---
+++ Zahran Razaq Wiryawan/contoh5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a = 10, b = 10, c = 10, d = 10, e = 10;
+cout << "Operator assignment gabungan bahasa C++" << endl;
+cout << "========================================" <<
+endl;
+cout << "Variabel a, b, c, d, e = 10" << endl;
+cout << endl;
+a += 5;
+b -= 3;
+c *= 3;
+d /= 3;
+e %= 3;
+cout << "Hasil operasi a += 5: " << a << endl;
+cout << "Hasil operasi b -= 3: " << b << endl;
+cout << "Hasil operasi c *= 3: " << c << endl;
+cout << "Hasil operasi d /= 3: " << d << endl;
+cout << "Hasil operasi e %= 3: " << e << endl;
+return 0;
+}
